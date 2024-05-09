283b7061a89476cbd57f5e24c271714e7bb01e55
kellyihyeon <kellykang13@gmail.com>
2024-05-09 17:08:19 +0900

    fix: 가계부 엔드포인트 복수형으로 수정

--- src/main/java/com/github/kellyihyeon/stanceadmin/presentation/accountbook/AccountBookController.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/presentation/accountbook/AccountBookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/account-book")
+@RequestMapping("/account-books")
 @RequiredArgsConstructor
 public class AccountBookController {
 
