a4a097cbfb12993738b79e09288f767015330d83
kellyihyeon <kellykang13@gmail.com>
2024-07-16 00:07:22 +0900

    add: 계좌 도메인 모델 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/account/Account.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/account/Account.java
@@ -1,4 +1,42 @@
 package com.github.kellyihyeon.stanceadmin.domain.account;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account {
+
+    private Long id;
+
+    private boolean isDefault;
+
+    private String accountNickname;
+
+    private Double balance;
+
+    private String accountHolder;
+
+    private String accountNumber;
+
+    private Bank bank;
+
+    private LocalDate usageStartDate;
+
+    private LocalDate usageEndDate;
+
+    private AccountStatus accountStatus;
+
+    private LocalDateTime createdAt;
+
+    private Long creatorId;
+
+    private LocalDateTime updatedAt;
+
+    private Long updaterId;
+
 }
