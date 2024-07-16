19462e8ad72670c21aa57a4fd877653527ca0497
kellyihyeon <kellykang13@gmail.com>
2024-07-16 17:49:34 +0900

    add: 계좌 데이터 유효성 검사를 위해 생성자에서 null 검사 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/account/Account.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/account/Account.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,7 +14,7 @@ public class Account {
 
     private Long id;
 
-    private boolean isDefault;
+    private Boolean isDefault;
 
     private String accountNickname;
 
@@ -39,4 +40,23 @@ public class Account {
 
     private Long updaterId;
 
+    public Account(Boolean isDefault, String accountNickname, Double balance, String accountHolder, String accountNumber, Bank bank, LocalDate usageStartDate, AccountStatus accountStatus, LocalDateTime createdAt, Long creatorId) {
+        requireNonNull(isDefault, "'기본 계좌 여부' 는 null 이어서는 안됩니다.");
+        requireNonNull(accountNickname, "'계좌 별칭' 은 null 이어서는 안됩니다.");
+        requireNonNull(balance, "'잔액' 은 null 이어서는 안됩니다.");
+        requireNonNull(accountHolder, "'계좌 소유주' 는 null 이어서는 안됩니다.");
+        requireNonNull(accountNumber, "'계좌번호' 는 null 이어서는 안됩니다.");
+        requireNonNull(bank, "'은행명' 은 null 이어서는 안됩니다.");
+        requireNonNull(usageStartDate, "'사용일' 은 null 이어서는 안됩니다.");
+        requireNonNull(accountStatus, "'계좌 상태' 는 null 이어서는 안됩니다.");
+        requireNonNull(createdAt, "'데이터 생성일' 은 null 이어서는 안됩니다.");
+        requireNonNull(creatorId, "'데이터 생성자 ID' 는 null 이어서는 안됩니다.");
+    }
+
+    private <T> T requireNonNull(T obj, String message) {
+        if (Objects.isNull(obj)) {
+            throw new IllegalArgumentException(message);
+        }
+        return obj;
+    }
 }
