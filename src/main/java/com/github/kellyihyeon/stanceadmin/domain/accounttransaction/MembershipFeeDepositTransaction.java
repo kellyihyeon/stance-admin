e68be8b7935ddfd9be31df833e197d84da6efb8c
kellyihyeon <kellykang13@gmail.com>
2024-07-18 17:13:32 +0900

    add: 회비입금내역 도메인 모델 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/accounttransaction/MembershipFeeDepositTransaction.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/accounttransaction/MembershipFeeDepositTransaction.java
@@ -1,7 +1,46 @@
 package com.github.kellyihyeon.stanceadmin.domain.accounttransaction;
 
+import com.github.kellyihyeon.stanceadmin.domain.member.MemberType;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MembershipFeeDepositTransaction {
+
+    private Long id;
+
+    private Long depositorId;
+
+    private LocalDate depositDate;
+
+    private Double amount;
+
+    private LocalDate dueDate;
+
+    private MemberType memberType;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private Long creatorId;
+
+    private LocalDateTime updatedAt;
+
+    private Long updaterId;
+
+    public MembershipFeeDepositTransaction(Long depositorId, LocalDate depositDate, Double amount, LocalDate dueDate, MemberType memberType, LocalDateTime createdAt, Long creatorId) {
+        this.depositorId = depositorId;
+        this.depositDate = depositDate;
+        this.amount = amount;
+        this.dueDate = dueDate;
+        this.memberType = memberType;
+        this.createdAt = createdAt;
+        this.creatorId = creatorId;
+    }
 }
