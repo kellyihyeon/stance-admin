3a0bb38c859fb733f562d8606b27d9d50d3321d1
kellyihyeon <kellykang13@gmail.com>
2024-07-19 22:38:03 +0900

    add: 이벤트 영속성 모델 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/infrastructure/repository/event/EventEntity.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/infrastructure/repository/event/EventEntity.java
@@ -1,13 +1,43 @@
 package com.github.kellyihyeon.stanceadmin.infrastructure.repository.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.github.kellyihyeon.stanceadmin.domain.event.EventItem;
+import com.github.kellyihyeon.stanceadmin.domain.event.EventStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EventEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_item", nullable = false)
+    private EventItem eventItem;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private EventStatus status;
+
+    public EventEntity(EventItem eventItem, Double amount, LocalDate dueDate, String description, EventStatus status) {
+        this.eventItem = eventItem;
+        this.amount = amount;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.status = status;
+    }
 }
