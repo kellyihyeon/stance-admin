c4c4496296547f23358907952ba49155baa9bdd2
kellyihyeon <kellykang13@gmail.com>
2024-07-20 00:17:43 +0900

    add: 이벤트 신청자 service dto 구현

--- src/main/java/com/github/kellyihyeon/stanceadmin/application/eventapplicant/dto/EventApplicantCreation.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/eventapplicant/dto/EventApplicantCreation.java
@@ -1,5 +1,20 @@
 package com.github.kellyihyeon.stanceadmin.application.eventapplicant.dto;
 
-public class EventApplicantCreation {
+import com.github.kellyihyeon.stanceadmin.domain.eventapplicant.DepositStatus;
 
+import java.util.List;
+
+public record EventApplicantCreation (
+        Long eventId,
+        List<Long> applicantIds,
+        String description,
+        DepositStatus depositStatus
+)
+{
+    public EventApplicantCreation(Long eventId, List<Long> applicantIds, String description, DepositStatus depositStatus) {
+        this.eventId = eventId;
+        this.applicantIds = applicantIds;
+        this.description = description;
+        this.depositStatus = depositStatus;
+    }
 }
--- src/main/java/com/github/kellyihyeon/stanceadmin/presentation/eventapplicant/EventApplicantController.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/presentation/eventapplicant/EventApplicantController.java
@@ -3,6 +3,7 @@ package com.github.kellyihyeon.stanceadmin.presentation.eventapplicant;
 import com.github.kellyihyeon.stanceadmin.apis.EventApplicantApi;
 import com.github.kellyihyeon.stanceadmin.application.eventapplicant.EventApplicantService;
 import com.github.kellyihyeon.stanceadmin.application.eventapplicant.dto.EventApplicantCreation;
+import com.github.kellyihyeon.stanceadmin.domain.eventapplicant.DepositStatus;
 import com.github.kellyihyeon.stanceadmin.models.EventApplicantInput;
 import com.github.kellyihyeon.stanceadmin.models.EventPayers;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +27,12 @@ public class EventApplicantController implements EventApplicantApi {
     @Override
     public ResponseEntity<Void> saveEventApplicant(EventApplicantInput eventApplicantInput) {
         EventApplicantCreation eventApplicantCreation = new EventApplicantCreation(
-
+                eventApplicantInput.getEventId(),
+                eventApplicantInput.getApplicantIds(),
+                eventApplicantInput.getDescription(),
+                DepositStatus.valueOf(eventApplicantInput.getDepositStatus().getValue())
         );
+
         eventApplicantService.createEventApplicant(eventApplicantCreation);
         return ResponseEntity.created(URI.create("")).build();
     }
