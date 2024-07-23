83ed121f962048836198fdf915ed03b9744b8f72
kellyihyeon <kellykang13@gmail.com>
2024-07-23 19:00:31 +0900

    feat: EventDepositTransaction 도메인 모델에 null 체크 로직 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/eventdeposittransaction/EventDepositTransaction.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/eventdeposittransaction/EventDepositTransaction.java
@@ -4,12 +4,19 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 public class EventDepositTransaction {
 
 
     public static EventDepositTransaction create(Long eventId, Long applicantId, Double amount, LocalDate depositDate, String description, Long creatorId, LocalDateTime createdAt) {
+        Objects.requireNonNull(eventId, "eventId 는 null 이어선 안됩니다.");
+        Objects.requireNonNull(applicantId, "applicantId 는 null 이어선 안됩니다.");
+        Objects.requireNonNull(amount, "amount 는 null 이어선 안됩니다.");
+        Objects.requireNonNull(depositDate, "depositDate 는 null 이어선 안됩니다.");
+        Objects.requireNonNull(creatorId, "creatorId 는 null 이어선 안됩니다.");
+        Objects.requireNonNull(createdAt, "createdAt 은 null 이어선 안됩니다.");
         return null;
     }
 }
