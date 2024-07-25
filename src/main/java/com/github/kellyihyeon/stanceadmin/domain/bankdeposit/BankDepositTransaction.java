ade23abbb29ca11ca5db4f0cafc419889f53d2df
kellyihyeon <kellykang13@gmail.com>
2024-07-25 16:44:26 +0900

    add: 도메인 모델 추가

---
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/bankdeposit/BankDepositTransaction.java
@@ -0,0 +1,67 @@
+package com.github.kellyihyeon.stanceadmin.domain.bankdeposit;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BankDepositTransaction {
+
+    private Long id;
+
+    private DepositType depositType;
+
+    private Long depositorId;
+
+    private Double amount;
+
+    private LocalDate depositDate;
+
+    public BankDepositTransaction(Long id, DepositType depositType, Long depositorId, Double amount, LocalDate depositDate) {
+        this.id = id;
+        this.depositType = depositType;
+        this.depositorId = depositorId;
+        this.amount = amount;
+        this.depositDate = depositDate;
+    }
+
+    private static BankDepositTransaction deposit(Long id, DepositType type, Long depositorId, Double amount, LocalDate depositDate) {
+        Objects.requireNonNull(id, "id 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(type, "type 이 null 이어선 안됩니다.");
+        Objects.requireNonNull(depositorId, "depositorId 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(amount, "amount 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(depositDate, "depositDate 가 null 이어선 안됩니다.");
+
+        return new BankDepositTransaction(
+                id,
+                type,
+                depositorId,
+                amount,
+                depositDate
+        );
+    }
+
+    public BankDepositTransaction depositCashback(Long id, Long depositorId, Double amount, LocalDate depositDate) {
+        return BankDepositTransaction.deposit(
+                id,
+                DepositType.CASHBACK,
+                depositorId,
+                amount,
+                depositDate
+        );
+    }
+
+    public BankDepositTransaction depositInterest(Long id, Long depositorId, Double amount, LocalDate depositDate) {
+        return BankDepositTransaction.deposit(
+                id,
+                DepositType.INTEREST,
+                depositorId,
+                amount,
+                depositDate
+        );
+    }
+}
