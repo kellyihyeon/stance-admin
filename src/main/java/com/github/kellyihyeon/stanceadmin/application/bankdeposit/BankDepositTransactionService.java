a28312190bc7e0351aa62b9bfddca5eb21eacd68
kellyihyeon <kellykang13@gmail.com>
2024-07-26 14:28:46 +0900

    add: service에 통장입금내역 저장 로직 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/application/bankdeposit/BankDepositTransactionService.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/bankdeposit/BankDepositTransactionService.java
@@ -1,10 +1,15 @@
 package com.github.kellyihyeon.stanceadmin.application.bankdeposit;
 
+import com.github.kellyihyeon.stanceadmin.application.accounttransaction.AccountTransactionService;
 import com.github.kellyihyeon.stanceadmin.application.bankdeposit.dto.BankDepositCreation;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionIdentity;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionSubType;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionType;
 import com.github.kellyihyeon.stanceadmin.domain.bankdeposit.BankDepositTransaction;
 import com.github.kellyihyeon.stanceadmin.domain.bankdeposit.BankDepositTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -15,11 +20,18 @@ public class BankDepositTransactionService {
     private final BankDepositTransactionRepository repository;
     private final BankDepositTransactionMapper mapper;
 
+    private final AccountTransactionService accountTransactionService;
+
+
+    @Transactional
     public void saveBankDepositTransaction(BankDepositCreation serviceDto) {
         Long loggedInId = 999L;
         LocalDateTime now = LocalDateTime.now();
 
         BankDepositTransaction bankDepositTransaction = mapper.toDomain(serviceDto, loggedInId, now);
-        Long transactionId = repository.saveBankDepositTransaction(bankDepositTransaction).getId();
+        BankDepositTransaction persistedBankDepositTransaction = repository.saveBankDepositTransaction(bankDepositTransaction);
+
+        TransactionIdentity transactionIdentity = TransactionIdentity.create(persistedBankDepositTransaction.getId(), TransactionType.DEPOSIT, TransactionSubType.BANK);
+        accountTransactionService.saveDepositAccountTransaction(transactionIdentity, persistedBankDepositTransaction.getAmount());
     }
 }
