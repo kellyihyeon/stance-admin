a54580f2ad202a94690d57620cd0ba676ec29866
kellyihyeon <kellykang13@gmail.com>
2024-07-27 00:59:14 +0900

    add: service에서 입금일 데이터 유효성 검증

--- src/main/java/com/github/kellyihyeon/stanceadmin/application/membershipfeedeposit/MembershipFeeDepositTransactionService.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/membershipfeedeposit/MembershipFeeDepositTransactionService.java
@@ -10,6 +10,17 @@ import java.util.List;
 public class MembershipFeeDepositTransactionService {
 
     public List<DepositRateResponse> getDepositRate(DepositDateCondition depositDateCondition) {
+        validateDepositDate(depositDateCondition);
         return null;
+    }
+
+    private void validateDepositDate(DepositDateCondition depositDateCondition) {
+        if (depositDateCondition.year() < 2023) {
+            throw new IllegalArgumentException("year 를 2023년 이상으로 지정해주세요.");
+        }
+
+        if (depositDateCondition.month() < 1 || depositDateCondition.month() > 12) {
+            throw new IllegalArgumentException("month 는 1에서 12 사이 이어야 해요.");
+        }
     }
 }
---
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/membershipfeedeposit/MembershipFeeDepositRepository.java
@@ -0,0 +1,4 @@
+package com.github.kellyihyeon.stanceadmin.domain.membershipfeedeposit;
+
+public interface MembershipFeeDepositRepository {
+}
