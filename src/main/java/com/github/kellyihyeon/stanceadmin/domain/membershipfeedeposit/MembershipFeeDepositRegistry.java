2e6741a79913fc8c1560f34f5dafac23059373c5
kellyihyeon <kellykang13@gmail.com>
2024-07-30 18:57:28 +0900

    add: 회비 입금 명단 도메인 모델에 팩터리 메서드 구현

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/membershipfeedeposit/MembershipFeeDepositRegistry.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/membershipfeedeposit/MembershipFeeDepositRegistry.java
@@ -4,6 +4,7 @@ import com.github.kellyihyeon.stanceadmin.domain.member.MemberStatus;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 public class MembershipFeeDepositRegistry {
@@ -22,7 +23,31 @@ public class MembershipFeeDepositRegistry {
 
     private LocalDate depositDate;
 
+
+    private MembershipFeeDepositRegistry(Long memberId, String memberName, Double amount, MemberStatus memberStatus, LocalDate dueDate, LocalDate depositDate) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.amount = amount;
+        this.memberStatus = memberStatus;
+        this.dueDate = dueDate;
+        this.depositDate = depositDate;
+    }
+
     public static MembershipFeeDepositRegistry create(Long memberId, String memberName, Double amount, MemberStatus memberStatus, LocalDate dueDate, LocalDate depositDate) {
-        return null;
+        Objects.requireNonNull(memberId, "memberId 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(memberName, "memberName 이 null 이어선 안됩니다.");
+        Objects.requireNonNull(amount, "amount 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(memberStatus, "memberStatus 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(dueDate, "dueDate 가 null 이어선 안됩니다.");
+        Objects.requireNonNull(depositDate, "depositDate 가 null 이어선 안됩니다.");
+
+        return new MembershipFeeDepositRegistry(
+                memberId,
+                memberName,
+                amount,
+                memberStatus,
+                dueDate,
+                depositDate
+        );
     }
 }
