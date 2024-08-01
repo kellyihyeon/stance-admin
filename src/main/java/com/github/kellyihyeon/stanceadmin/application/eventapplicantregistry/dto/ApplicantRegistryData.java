7d1e26bab0ee51f3accb06c2637d840ff14b72ae
kellyihyeon <kellykang13@gmail.com>
2024-08-01 15:32:46 +0900

    feat: queryDsl을 사용하여 조회 쿼리 작성

---
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/eventapplicantregistry/dto/ApplicantRegistryData.java
@@ -0,0 +1,31 @@
+package com.github.kellyihyeon.stanceadmin.application.eventapplicantregistry.dto;
+
+import com.github.kellyihyeon.stanceadmin.domain.eventapplicantregistry.DepositStatus;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@ToString
+public class ApplicantRegistryData {
+    private String eventDescription;
+
+    private LocalDate dueDate;
+
+    private String memberName;
+
+    private Double amount;
+
+    private DepositStatus depositStatus;
+
+    @QueryProjection
+    public ApplicantRegistryData(String eventDescription, LocalDate dueDate, String memberName, Double amount, DepositStatus depositStatus) {
+        this.eventDescription = eventDescription;
+        this.dueDate = dueDate;
+        this.memberName = memberName;
+        this.amount = amount;
+        this.depositStatus = depositStatus;
+    }
+}
--- src/main/java/com/github/kellyihyeon/stanceadmin/infrastructure/repository/eventapplicantregistry/EventApplicantRegistryRepositoryImpl.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/infrastructure/repository/eventapplicantregistry/EventApplicantRegistryRepositoryImpl.java
@@ -1,13 +1,20 @@
 package com.github.kellyihyeon.stanceadmin.infrastructure.repository.eventapplicantregistry;
 
 import com.github.kellyihyeon.stanceadmin.application.eventapplicantregistry.EventApplicantRegistryMapper;
+import com.github.kellyihyeon.stanceadmin.application.eventapplicantregistry.dto.ApplicantRegistryData;
+import com.github.kellyihyeon.stanceadmin.application.eventapplicantregistry.dto.QApplicantRegistryData;
 import com.github.kellyihyeon.stanceadmin.domain.eventapplicantregistry.EventApplicantDepositRegistry;
 import com.github.kellyihyeon.stanceadmin.domain.eventapplicantregistry.EventApplicantRegistry;
 import com.github.kellyihyeon.stanceadmin.domain.eventapplicantregistry.EventApplicantRegistryRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.github.kellyihyeon.stanceadmin.infrastructure.entity.member.QMemberEntity.memberEntity;
+import static com.github.kellyihyeon.stanceadmin.infrastructure.repository.event.QEventEntity.eventEntity;
+import static com.github.kellyihyeon.stanceadmin.infrastructure.repository.eventapplicantregistry.QEventApplicantRegistryEntity.eventApplicantRegistryEntity;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,6 +22,9 @@ public class EventApplicantRegistryRepositoryImpl implements EventApplicantRegis
 
     private final JpaEventApplicantRegistryEntityRepository jpaRepository;
     private final EventApplicantRegistryMapper mapper;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
 
     @Override
     public void saveEventApplicant(EventApplicantRegistry eventApplicantRegistry) {
@@ -37,6 +47,22 @@ public class EventApplicantRegistryRepositoryImpl implements EventApplicantRegis
 
     @Override
     public List<EventApplicantDepositRegistry> getEventApplicantRegistriesByEventId(Long eventId) {
+        List<ApplicantRegistryData> applicantRegistryDataDto = jpaQueryFactory
+                .select(new QApplicantRegistryData(
+                        eventEntity.description,
+                        eventEntity.dueDate,
+                        memberEntity.name,
+                        eventEntity.amount,
+                        eventApplicantRegistryEntity.depositStatus
+                ))
+                .from(eventApplicantRegistryEntity)
+                .leftJoin(eventEntity)
+                    .on(eventApplicantRegistryEntity.eventId.eq(eventEntity.id))
+                .leftJoin(memberEntity)
+                    .on(eventApplicantRegistryEntity.applicantId.eq(memberEntity.id))
+                .where(eventApplicantRegistryEntity.eventId.eq(eventId))
+                .fetch();
+
         return null;
     }
 
