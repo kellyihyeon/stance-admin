33b44ccec3e1e8b307070af63264df9fbcca92fc
kellyihyeon <kellykang13@gmail.com>
2024-09-09 11:18:50 +0900

    feat: 계좌이체 내역 저장 후 입출금내역 저장

--- src/main/java/com/github/kellyihyeon/stanceadmin/application/transfertransaction/TransferTransactionService.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/transfertransaction/TransferTransactionService.java
@@ -1,9 +1,15 @@
 package com.github.kellyihyeon.stanceadmin.application.transfertransaction;
 
+import com.github.kellyihyeon.stanceadmin.application.accounttransaction.AccountTransactionService;
 import com.github.kellyihyeon.stanceadmin.application.transfertransaction.dto.TransferCreation;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionIdentity;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionSubType;
+import com.github.kellyihyeon.stanceadmin.domain.accounttransaction.TransactionType;
+import com.github.kellyihyeon.stanceadmin.domain.transfertransaction.TransferTransaction;
 import com.github.kellyihyeon.stanceadmin.domain.transfertransaction.TransferTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -14,10 +20,20 @@ public class TransferTransactionService {
     private final TransferTransactionRepository repository;
     private final TransferTransactionMapper mapper;
 
+    private final AccountTransactionService accountTransactionService;
+
+    @Transactional
     public void saveTransferTransaction(TransferCreation serviceDto) {
         LocalDateTime now = LocalDateTime.now();
         Long loggedInId = 999L;
 
-        repository.save(mapper.toDomain(serviceDto, loggedInId, now));
+        TransferTransaction transferTransaction = repository.save(mapper.toDomain(serviceDto, loggedInId, now));
+
+        TransactionIdentity transactionIdentity = TransactionIdentity.create(
+                transferTransaction.getId(),
+                TransactionType.WITHDRAW,
+                TransactionSubType.TRANSFER
+        );
+        accountTransactionService.saveAccountTransaction(transactionIdentity, transferTransaction.getAmount().doubleValue());
     }
 }
