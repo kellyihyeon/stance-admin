edd58ceec5927e3fa2d27aa23fb26df22db14fd8
kellyihyeon <kellykang13@gmail.com>
2024-09-09 14:36:34 +0900

    add: 도메인 모델 추가

--- src/main/java/com/github/kellyihyeon/stanceadmin/domain/cardpayment/CardPaymentTransaction.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/domain/cardpayment/CardPaymentTransaction.java
@@ -1,9 +1,14 @@
 package com.github.kellyihyeon.stanceadmin.domain.cardpayment;
 
+import com.github.kellyihyeon.stanceadmin.domain.transfertransaction.ExpenseCategory;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @ToString
@@ -11,4 +16,25 @@ import lombok.ToString;
 public class CardPaymentTransaction {
 
     private Long id;
+
+    private Long cardHolderId;
+
+    private ExpenseCategory expenseCategory;
+
+    private String cardUsageLocation;
+
+    private BigDecimal amount;
+
+    private LocalDate expenseDate;
+
+    private String description;
+
+    private Long creatorId;
+
+    private LocalDateTime createdAt;
+
+    private Long updaterId;
+
+    private LocalDateTime updatedAt;
+
 }
