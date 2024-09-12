1545a7b3fed1c125b7000d4e3dbb96ac8315362f
kellyihyeon <kellykang13@gmail.com>
2024-09-12 19:12:43 +0900

    add: 조회 전용 서비스에 쿼리 결과를 응답값으로 변환

--- src/main/java/com/github/kellyihyeon/stanceadmin/application/accounttransaction/AccountTransactionQueryService.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/application/accounttransaction/AccountTransactionQueryService.java
@@ -2,12 +2,16 @@ package com.github.kellyihyeon.stanceadmin.application.accounttransaction;
 
 import com.github.kellyihyeon.stanceadmin.infrastructure.querydsl.AccountTransactionProjection;
 import com.github.kellyihyeon.stanceadmin.infrastructure.repository.accounttransaction.AccountTransactionQueryRepository;
+import com.github.kellyihyeon.stanceadmin.models.AccountTransactionResponse;
 import com.github.kellyihyeon.stanceadmin.models.PagedAccountTransactionResponse;
+import com.github.kellyihyeon.stanceadmin.models.PagingMetadata;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +22,30 @@ public class AccountTransactionQueryService {
     public PagedAccountTransactionResponse getAllAccountTransactions(PageRequest pageable) {
         List<AccountTransactionProjection> projections = queryRepository.findAccountTransactionWithJoins(pageable);
 
-        // convert projections to PagedAccountTransactionResponse
+        List<AccountTransactionResponse> allAccountTransactions = projections.stream()
+                .map(projection -> {
+                    AccountTransactionResponse response = new AccountTransactionResponse();
+                    response.id(projection.getId());
+                    response.transactionId(projection.getTransactionId());
+                    response.transactionType(projection.getTransactionType().name());
+                    response.detailType(projection.getDetailType());
+                    response.transactionParty(projection.getTransactionParty());
+                    response.amount(projection.getAmount());
+                    response.balance(projection.getBalance());
 
-        return new PagedAccountTransactionResponse();
+                    return response;
+                })
+                .collect(Collectors.toList());
 
+
+        PageImpl<AccountTransactionResponse> pagedTransactions = new PageImpl<>(allAccountTransactions, pageable, allAccountTransactions.size());
+        PagingMetadata pagingMetadata = new PagingMetadata(
+                pagedTransactions.getNumber(),
+                pagedTransactions.getSize(),
+                pagedTransactions.getTotalElements(),
+                pagedTransactions.getTotalPages()
+        );
+
+        return new PagedAccountTransactionResponse(allAccountTransactions, pagingMetadata);
     }
 }
