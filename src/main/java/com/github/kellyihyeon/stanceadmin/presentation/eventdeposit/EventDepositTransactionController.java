c7863690c8d9002a266cd2cfd30bf5e9f837a465
kellyihyeon <kellykang13@gmail.com>
2024-09-24 16:02:35 +0900

    fix: 이벤트 입금 내역 등록 API 엔드포인트 수정

--- src/main/java/com/github/kellyihyeon/stanceadmin/presentation/eventdeposit/EventDepositTransactionController.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/presentation/eventdeposit/EventDepositTransactionController.java
@@ -1,9 +1,9 @@
 package com.github.kellyihyeon.stanceadmin.presentation.eventdeposit;
 
-import com.github.kellyihyeon.stanceadmin.apis.EventDepositTransactionApi;
-import com.github.kellyihyeon.stanceadmin.application.eventdeposit.dto.EventDepositCreation;
+import com.github.kellyihyeon.stanceadmin.apis.EventFeeDepositTransactionApi;
 import com.github.kellyihyeon.stanceadmin.application.eventdeposit.EventDepositTransactionService;
-import com.github.kellyihyeon.stanceadmin.models.EventDepositInput;
+import com.github.kellyihyeon.stanceadmin.application.eventdeposit.dto.EventDepositCreation;
+import com.github.kellyihyeon.stanceadmin.models.EventFeeDepositInput;
 import com.github.kellyihyeon.stanceadmin.presentation.TimeConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +13,12 @@ import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
-public class EventDepositTransactionController implements EventDepositTransactionApi {
+public class EventDepositTransactionController implements EventFeeDepositTransactionApi {
 
     private final EventDepositTransactionService eventDepositService;
 
     @Override
-    public ResponseEntity<Void> saveEventDepositTransaction(EventDepositInput input) {
+    public ResponseEntity<Void> saveEventFeeDepositTransaction(EventFeeDepositInput input) {
         EventDepositCreation serviceDto = EventDepositCreation.create(
                 input.getEventId(),
                 input.getDepositorIds(),
