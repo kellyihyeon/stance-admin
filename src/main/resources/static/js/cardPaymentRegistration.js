5b960229e8b9034e900263a155cbbdb5f8f26102
kellyihyeon <kellykang13@gmail.com>
2024-09-25 01:26:22 +0900

    add: 서버에 카드 지출 기록 저장하는 api 구현

--- src/main/resources/static/js/cardPaymentRegistration.js
+++ src/main/resources/static/js/cardPaymentRegistration.js
@@ -20,5 +20,42 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(document.getElementById('cardHolderId').value)
         });
 
+        document.getElementById('cardPaymentForm').addEventListener('submit', function (event) {
+            const cardPaymentRegistrationModal = new bootstrap.Modal(document.getElementById('cardPaymentRegistrationModal'));
+            event.preventDefault();
+
+            const bodyData = {
+                cardHolderId: document.getElementById('cardHolderId').value,
+                expenseCategory: document.getElementById('expenseCategory').value,
+                cardUsageLocation: document.getElementById('cardUsageLocation').value,
+                amount: document.getElementById('cardPaymentCost').value,
+                expenseDate: document.getElementById('cardPaymentDate').value,
+                description: document.getElementById('cardPaymentMemo').value
+            };
+
+            const url = '/account-transactions/withdrawals/card-payment';
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(bodyData)
+            })
+                .then(response => {
+                    if (response.status === 201) {
+                        alert('카드 지출이 등록되었어요!');
+                        cardPaymentRegistrationModal.hide();
+                        window.location.href = '/budget-book-registration';
+                    } else {
+                        response.json().then(errorData => {
+                            throw new Error(`Error ${response.status}: ${errorData.message || 'Unknown error'}`)
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.error('카드 지출 등록 중 오류 발생:', error);
+                });
+        });
+
     }
 });
