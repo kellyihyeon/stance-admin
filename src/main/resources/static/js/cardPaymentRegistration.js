0234dc5d14aecc3038bb5f87d4a4dead634b43a9
kellyihyeon <kellykang13@gmail.com>
2024-09-25 01:16:11 +0900

    fix: 카드 지출 기록 모달-카드소유자 태그를 select box로 변경

--- src/main/resources/static/js/cardPaymentRegistration.js
+++ src/main/resources/static/js/cardPaymentRegistration.js
@@ -3,24 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (path === '/budget-book-registration') {
         document.getElementById('cardPaymentRegistrationModal').addEventListener('shown.bs.modal', function () {
+            const cardHolderSelectBox = document.getElementById('cardHolderId');
+
             getCurrentMembers()
                 .then(data => {
-                    const cardHolderContainer = document.getElementById('cardHolderContainer');
-                    cardHolderContainer.innerHTML = '';
-
-                    data.forEach(currentMember => {
-                            const checkbox = document.createElement('div');
-                            checkbox.classList.add('form-check');
-                            checkbox.innerHTML = `
-                                <input class="form-check-input" type="checkbox" value="${currentMember.memberId}" id="cardHolder${currentMember.memberId}">
-                                <label class="form-check-label" for="cardHolder${currentMember.memberId}">${currentMember.memberName}</label>
-                             `;
-                        cardHolderContainer.appendChild(checkbox);
+                    data.forEach(member => {
+                            const option = document.createElement('option');
+                            option.value = member.memberId;
+                            option.textContent = member.memberName;
+                            cardHolderSelectBox.appendChild(option)
                         }
                     );
-
                 })
-                .catch(error => console.error('/members/current 호출 중 에러 발생:', error));
+                .catch(error => console.error('/members/current 호출 중 에러 발생: ', error))
+
+            console.log(document.getElementById('cardHolderId').value)
         });
+
     }
 });
