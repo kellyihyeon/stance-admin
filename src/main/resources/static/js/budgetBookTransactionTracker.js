658736254bebab63a3938392667c36623e30d147
kellyihyeon <kellykang13@gmail.com>
2024-09-26 00:00:29 +0900

    add: 시스템-모임통장 전체 내역 API 호출

--- src/main/resources/static/js/budgetBookTransactionTracker.js
+++ src/main/resources/static/js/budgetBookTransactionTracker.js
@@ -36,6 +36,40 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('view-all-transactions-button').addEventListener('click', function () {
             window.location.href = '/budget-book-transaction-tracker';
         });
+    }
+
+    if (path === '/budget-book-transaction-tracker') {
+        getAllAccountTransactions()
+            .then(data => {
+                try {
+                    const tableBody = document.getElementById('transaction-table-body');
+                    const transactions = data.content;
+
+                    tableBody.innerHTML = '';
+                    transactions.forEach(transaction => {
+
+                        let transactionType = getTransactionType(transaction.transactionType)
+                        let amount = transaction.transactionType === '출금'
+                            ? `- ${transaction.amount.toLocaleString()}원`
+                            : `${transaction.amount.toLocaleString()}원`;
+
+                        const row = `
+                    <tr>
+                        <td>${transaction.transactionDate}</td>
+                        <td><span class="transaction-type ${transactionType}">${transaction.transactionType}</span></td>
+                        <td>${transaction.detailType}</td>
+                        <td>${transaction.transactionParty}</td>
+                        <td><span class="amount ${transactionType}">${amount}</span></td>
+                        <td>${transaction.balance.toLocaleString()}원</td>
+                    </tr>
+                `;
+                        tableBody.innerHTML += row;
+                    });
+                } catch (error) {
+                    console.error('API 데이터를 불러오는 중 오류가 발생했습니다:', error);
+                }
+
+            });
 
     }
 });
