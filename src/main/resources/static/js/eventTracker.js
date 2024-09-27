12f19963afb889a311e75a433a3b168335eb9dc3
kellyihyeon <kellykang13@gmail.com>
2024-09-28 00:03:28 +0900

    style: 이벤트명에 이미지 및 스타일 적용

--- src/main/resources/static/js/eventTracker.js
+++ src/main/resources/static/js/eventTracker.js
@@ -35,19 +35,31 @@ function loadEvents(page) {
                 const events = data.content;
                 tableBody.innerHTML = '';
 
+                const imgUrls = {
+                    "회식": "images/team-party.png",
+                    "유니폼": "images/basketball-jersey.png",
+                    "엠티": "images/membership-training.png"
+                };
+
                 events.forEach(event => {
                     const formattedCreatedAt = removeSeconds(`${event.createdAt}`);
-                    const status = `${event.status}`;
+                    const eventStatusText = event.status === 'ACTIVE' ? "on" : "off";
+                    const creatorName = event.creatorName ? event.creatorName : '';
+                    const eventName = event.eventName
+                    const imgUrl = imgUrls[eventName];
 
                     const row = `
                             <tr>
-                                <td>${event.eventName}</td>
+                                <td>
+                                    <img src="${imgUrl}" alt="${eventName} 이미지" style="width: 30px; height: 30px; margin-right: 8px;">
+                                    <strong>${event.eventName}</strong>
+                                </td>
                                 <td>${event.description}</td>
                                 <td>${event.amount.toLocaleString()}원</td>
                                 <td>${event.dueDate}</td>
                                 <td>${formattedCreatedAt}</td>
-                                <td>${event.creatorName}</td>
-                                <td>${status}</td>
+                                <td>${creatorName}</td>
+                                <td><span class="event-status ${event.status}">${eventStatusText}</span></td>
                             </tr>
                         `;
 
