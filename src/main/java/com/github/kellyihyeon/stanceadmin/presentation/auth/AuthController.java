f6a4ddc7c88eb15e2ecec03733567ba6aa1d79a0
kellyihyeon <kellykang13@gmail.com>
2024-10-01 17:44:36 +0900

    fix: openapi.yml 스펙 수정

--- src/main/java/com/github/kellyihyeon/stanceadmin/presentation/auth/AuthController.java
+++ src/main/java/com/github/kellyihyeon/stanceadmin/presentation/auth/AuthController.java
@@ -2,7 +2,7 @@ package com.github.kellyihyeon.stanceadmin.presentation.auth;
 
 import com.github.kellyihyeon.stanceadmin.apis.AuthApi;
 import com.github.kellyihyeon.stanceadmin.application.auth.AuthService;
-import com.github.kellyihyeon.stanceadmin.models.CheckAdminPermissionRequest;
+import com.github.kellyihyeon.stanceadmin.models.CheckSystemAdminPermissionRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +14,8 @@ public class AuthController implements AuthApi {
     private final AuthService service;
 
     @Override
-    public ResponseEntity<Void> checkAdminPermission(CheckAdminPermissionRequest checkAdminPermissionRequest) {
-        service.checkSystemAdminPermission(checkAdminPermissionRequest.getAdminKey());
+    public ResponseEntity<Void> checkSystemAdminPermission(CheckSystemAdminPermissionRequest checkSystemAdminPermissionRequest) {
+        service.checkSystemAdminPermission(checkSystemAdminPermissionRequest.getPermissionKey());
         return ResponseEntity.ok().build();
     }
 }
